db2f89ccae212e801cb121e2d4752098a9d15dd8
guy <guinildoob755@gmail>
2026-05-27 14:39:12 +0200

    firt_commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div>
+      <h1>Bienvenue sur mon projet 201_lab</h1>
+      <p>Premier test React réussi </p>
+    </div>
+  )
+} 
+
+
+export default App
